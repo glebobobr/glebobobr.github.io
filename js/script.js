d4aa7881258ab755418c8d22fa67104b147ddc1f
glebobobr <glebvoronin456@gmail.com>
2025-06-01 07:12:29 +0500

    редизайн (+анимашка)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-// Здесь можно добавлять JavaScript-логику для интерактивности
-console.log("Сайт загружен!");
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('modeToggle');
+    const iconLight = document.getElementById('iconLight');
+    const iconDark = document.getElementById('iconDark');
+    const body = document.body;
+
+    toggleButton.addEventListener('click', () => {
+        // Переключаем режим
+        const isLightMode = body.classList.toggle('light-mode');
+
+        if (isLightMode) {
+            // Переключение на светлый режим
+            iconDark.style.display = 'none';
+            iconLight.style.display = 'block';
+        } else {
+            // Переключение на темный режим
+            iconDark.style.display = 'block';
+            iconLight.style.display = 'none';
+        }
+    });
+});
